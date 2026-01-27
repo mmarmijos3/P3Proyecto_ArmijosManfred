226415276fab3381917de809cc146f058138da32
Manfred Armijos <Manfred Armijos@ALMM>
2026-01-27 00:07:42 -0500

    builder

---
+++ src/main/java/Model/Entities/CruiseShip.java
@@ -0,0 +1,45 @@
+package Model.Entities;
+
+/**
+ *
+ * @author Armijos Manfred, Balseca Valeska
+ */
+
+public class CruiseShip extends Vessel{
+    private int passengerCapacity;
+    private int passengers;
+
+    public CruiseShip(int passengerCapacity, int passengers, String name, String imo, double length, double bean, double draft, String type) {
+        super(name, imo, length, bean, draft, type);
+        this.passengerCapacity = passengerCapacity;
+        this.passengers = passengers;
+    }
+
+    public int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
+    public void setPassengerCapacity(int passengerCapacity) {
+        this.passengerCapacity = passengerCapacity;
+    }
+
+    public int getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(int passengers) {
+        this.passengers = passengers;
+    }
+
+    
+    @Override
+    public void boarding() {
+        setPassengers(getPassengerCapacity());
+    }
+
+    @Override
+    public void disembarkation() {
+        setPassengers(0);
+    }
+
+}
